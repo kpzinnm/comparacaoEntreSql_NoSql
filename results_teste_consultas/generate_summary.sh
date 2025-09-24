baeb23d7cc44cfd66841e686ea215f6c10274c98
Daniel Mantovani <103224655+dnielMantov@users.noreply.github.com>
2025-09-23 22:51:44 -0300

    Update generate_summary.sh

--- results_teste_consultas/generate_summary.sh
+++ results_teste_consultas/generate_summary.sh
@@ -2,26 +2,25 @@
 echo "=== RESUMO DOS RESULTADOS ==="
 echo ""
 echo "PostgreSQL Read Simple:"
-grep "transactions:" ./postgres_read_simple.log | tail -1
-grep "queries:" ./postgres_read_simple.log | tail -1
+grep "transactions:" ./results_teste_consultas/postgres_read_simple.log | tail -1
+grep "queries:" ./results_teste_consultas/postgres_read_simple.log | tail -1
 echo ""
 echo "PostgreSQL Read Range:"
-grep "transactions:" ./postgres_read_range.log | tail -1
-grep "queries:" ./postgres_read_range.log | tail -1
+grep "transactions:" ./results_teste_consultas/postgres_read_range.log | tail -1
+grep "queries:" ./results_teste_consultas/postgres_read_range.log | tail -1
 echo ""
 echo "PostgreSQL Read Join + Agg:"
-grep "transactions:" ./postgres_read_join_agg.log | tail -1
-grep "queries:" ./postgres_read_join_agg.log | tail -1
+grep "transactions:" ./results_teste_consultas/postgres_read_join_agg.log | tail -1
+grep "queries:" ./results_teste_consultas/postgres_read_join_agg.log | tail -1
 echo ""
 echo "MongoDB Read Simple:"
-grep "\[OVERALL\], Throughput" ./mongo_read_simple.log
-grep "\[READ\], AverageLatency" ./mongo_read_simple.log
+grep "\[OVERALL\], Throughput" ./results_teste_consultas/mongo_read_simple.log
+grep "\[READ\], AverageLatency" ./results_teste_consultas/mongo_read_simple.log
 echo ""
 echo "MongoDB Read Range:"
-grep "\[OVERALL\], Throughput" ./mongo_read_range.log
-grep "\[READ\], AverageLatency" ./mongo_read_range.log
+grep "\[OVERALL\], Throughput" ./results_teste_consultas/mongo_read_range.log
+grep "\[READ\], AverageLatency" ./results_teste_consultas/mongo_read_range.log
 echo ""
 echo "MongoDB Read Join Agg:"
-grep "\[OVERALL\], Throughput" ./mongo_read_join_agg.log
-grep "\[READ\], AverageLatency" ./mongo_read_join_agg.log
-grep "\[UPDATE\], AverageLatency" ./mongo_read_join_agg.log
+grep "\[OVERALL\], Throughput" ./results_teste_consultas/mongo_read_join_agg.log
+grep "\[READ\], AverageLatency" ./results_teste_consultas/mongo_read_join_agg.log
