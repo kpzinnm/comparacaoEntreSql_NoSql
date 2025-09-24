faddaf660ece550cf7f35bfd328e9204478fa572
kpzinnm <gabriel.barradev@gmail.com>
2025-09-24 00:14:37 -0300

    modificacoes

--- mongo/init/01-init.js
+++
@@ -1,5 +0,0 @@
-// Criar coleções para dados Olist
-db.createCollection("customers");
-db.createCollection("orders");
-db.createCollection("order_items");
-// Adicione outras coleções conforme necessário
